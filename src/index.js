13a1d7928bd6fe6a001bd7c3832415e8612f29db
wanderer120 <megamanx120@hotmail.com>
2022-10-28 00:40:43 +0800

    fix camera size

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ const App = () => {
       {!isVenueValidated&&(
         <div>Loading...</div>
       )}
-      <div>version:27.10.2022.4</div>
+      <div>version:27.10.2022.5</div>
     </Styles>
   </MuiThemeProvider>
 )}
